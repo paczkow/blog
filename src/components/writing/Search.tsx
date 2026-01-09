c24bd3deff96adf71567f8c949273e46a7cca434
Michał Bartosz Paczków <paczkow@meta.com>
2026-01-09 16:10:45 +0100

    Fix mobile layout on writing page
    - Wrap Search component content in flex container with lg:contents
    - Prevent input section from stretching on mobile with shrink-0
    - Articles section now takes remaining space with flex-1
    - Wrap back link and Search in flex container on writing page

--- src/components/writing/Search.tsx
+++ src/components/writing/Search.tsx
@@ -20,8 +20,8 @@ export const Search = ({
   const filteredPosts = getSortedByDate(search(query));
 
   return (
-    <>
-      <div className="sticky col-span-full mb-8 flex flex-col gap-4 lg:top-(--nav-height) lg:col-start-1 lg:col-end-2 lg:mb-0 lg:h-[calc(100vh-242px)]">
+    <div className="col-span-full flex flex-col flex-1 lg:contents">
+      <div className="shrink-0 mb-8 flex flex-col gap-4 lg:sticky lg:top-(--nav-height) lg:col-start-1 lg:col-end-2 lg:mb-0 lg:h-[calc(100vh-242px)]">
         <h2 className="text-sand-12 mb-4 font-semibold">Writing</h2>
         <p className="text-sand-11 mb-4">Find all my writing here</p>
         <SearchInput
@@ -31,11 +31,11 @@ export const Search = ({
         />
         {icons}
       </div>
-      <div className="col-span-full flex flex-col gap-12 lg:col-start-2 lg:col-end-3 mr-[2px]">
+      <div className="flex-1 flex flex-col gap-12 lg:col-start-2 lg:col-end-3 mr-[2px]">
         <section id="articles" className="flex flex-1 flex-col gap-12">
           <SearchList posts={filteredPosts} />
         </section>
       </div>
-    </>
+    </div>
   );
 };
